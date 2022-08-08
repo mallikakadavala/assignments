fdae9673182d44b6798f52f4707c4979671a55ed
Hasith Kadavala <hasithkadavala@Hasiths-MBP.attlocal.net>
2022-08-08 18:29:05 -0500

    example 88 file created

---
+++ example/ex_88.py
@@ -0,0 +1,26 @@
+first_name = "kirthana"                               # string
+lost_name = "kadali"                                  # string
+Age_in_years = "30"                                   # integer
+height_in_cm = "155"                                  # integer
+weight_in_kg = "54.85"                                # Float 
+marital_status = "true"                               # bool
+no_of_kids = None                                     # None type variable
+job_status = None                                     # None type variable
+living_in_usa = "texas"                               # string
+businesses = "false"                                  # bool
+
+description = "this is description"
+#about Mrs Kirthana 
+
+print (first_name)
+print (lost_name)
+print (Age_in_years)
+print (height_in_cm)
+print (weight_in_kg)
+print (marital_status)
+print (no_of_kids)
+print(type(no_of_kids))
+print (job_status)
+print (living_in_usa)
+print (businesses)
+
